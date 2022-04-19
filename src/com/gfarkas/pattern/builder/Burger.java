1f019b29589e8d8b563d27b7958dae9e4adec4ad
gabor <info@gfarkas.com>
2022-04-19 11:16:29 +0200

    - Builder Pattern 2.0

--- src/com/gfarkas/pattern/builder/Burger.java
+++ src/com/gfarkas/pattern/builder/Burger.java
@@ -1,25 +1,25 @@
 package com.gfarkas.pattern.builder;
 
 public class Burger {
-    private final String name;
-    private final int price;
-    private final Bun bun;
-    private final Patty patty;
-    private final Cheese cheese;
-    private final Sauce sauce;
-    private boolean salad;
-    private int tomato;
-    private int onion;
-    private int cucumber;
-    private int grilledBacon;
-    private int redOnion;
-    private int jalapeno;
-    private boolean mastard;
-    private boolean blueberryChatney;
-    private int bloomingOnion;
-    private int grilledOnion;
-    private int onionRings;
-    private int friedEggs;
+    private final String name; // required
+    private final int price; // required
+    private final Bun bun; // required
+    private final Patty patty; // required
+    private final Cheese cheese; // required
+    private final Sauce sauce; // required
+    private final boolean salad; // optional
+    private final int tomato; // optional
+    private final int onion; // optional
+    private final int cucumber; // optional
+    private final int grilledBacon; // optional
+    private final int redOnion; // optional
+    private final int jalapeno; // optional
+    private final boolean mastard; // optional
+    private final boolean blueberryChatney; // optional
+    private final int bloomingOnion; // optional
+    private final int grilledOnion; // optional
+    private final int onionRings; // optional
+    private final int friedEggs; // optional
 
     private Burger(BurgerBuilder burgerBuilder) {
         this.name = burgerBuilder.name;
@@ -41,6 +41,83 @@ public class Burger {
         this.grilledOnion = burgerBuilder.grilledOnion;
         this.onionRings = burgerBuilder.onionRings;
         this.friedEggs = burgerBuilder.friedEggs;
+    }
+    //All getter, and NO setter to provde immutability
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Bun getBun() {
+        return bun;
+    }
+
+    public Patty getPatty() {
+        return patty;
+    }
+
+    public Cheese getCheese() {
+        return cheese;
+    }
+
+    public Sauce getSauce() {
+        return sauce;
+    }
+
+    public boolean isSalad() {
+        return salad;
+    }
+
+    public int getTomato() {
+        return tomato;
+    }
+
+    public int getOnion() {
+        return onion;
+    }
+
+    public int getCucumber() {
+        return cucumber;
+    }
+
+    public int getGrilledBacon() {
+        return grilledBacon;
+    }
+
+    public int getRedOnion() {
+        return redOnion;
+    }
+
+    public int getJalapeno() {
+        return jalapeno;
+    }
+
+    public boolean isMastard() {
+        return mastard;
+    }
+
+    public boolean isBlueberryChatney() {
+        return blueberryChatney;
+    }
+
+    public int getBloomingOnion() {
+        return bloomingOnion;
+    }
+
+    public int getGrilledOnion() {
+        return grilledOnion;
+    }
+
+    public int getOnionRings() {
+        return onionRings;
+    }
+
+    public int getFriedEggs() {
+        return friedEggs;
     }
 
     public static class BurgerBuilder {
@@ -88,22 +165,28 @@ public class Burger {
             return this;
         }
 
+        public BurgerBuilder grilledBacon(int grilledBacon) {
+            this.grilledBacon = grilledBacon;
+            return this;
+        }
+
         public BurgerBuilder grilledOnion(int grilledOnion) {
-            this.grilledOnion = BurgerBuilder.this.grilledOnion;
+            this.grilledOnion = grilledOnion;
             return this;
         }
 
         public BurgerBuilder onionRings(int onionRings) {
-            this.onionRings = BurgerBuilder.this.onionRings;
+            this.onionRings = onionRings;
             return this;
         }
 
         public BurgerBuilder bloomingOnion(int bloomingOnion) {
-            this.bloomingOnion = BurgerBuilder.this.bloomingOnion;
+            this.bloomingOnion = bloomingOnion;
             return this;
         }
+
         public BurgerBuilder redOnion(int redOnion) {
-            this.redOnion = BurgerBuilder.this.redOnion;
+            this.redOnion = redOnion;
             return this;
         }
 
@@ -113,32 +196,33 @@ public class Burger {
             return this;
         }
 
-        public BurgerBuilder blueberryChatney(int blueberryChatney) {
-            this.blueberryChatney = BurgerBuilder.this.blueberryChatney;
+        public BurgerBuilder blueberryChatney(boolean blueberryChatney) {
+            this.blueberryChatney = blueberryChatney;
             return this;
         }
 
         public BurgerBuilder jalapeno(int jalapeno) {
-            this.jalapeno = BurgerBuilder.this.jalapeno;
+            this.jalapeno = jalapeno;
             return this;
         }
 
-        public BurgerBuilder mastard(int mastard) {
-            this.mastard = BurgerBuilder.this.mastard;
+        public BurgerBuilder mastard(boolean mastard) {
+            this.mastard = mastard;
             return this;
         }
 
         public BurgerBuilder friedEggs(int friedEggs) {
-            this.friedEggs = BurgerBuilder.this.friedEggs;
+            this.friedEggs = friedEggs;
             return this;
         }
 
-        public Burger build() throws InvalidBurgerException {
+        // Returns the finally constructed Burger object
+        public Burger build() {
             burgerValidator();
             return new Burger(this);
         }
 
-        private void burgerValidator() throws InvalidBurgerException {
+        private void burgerValidator() {
             StringBuilder sb = new StringBuilder();
             if (name == null) {
                 sb.append("Enter name of the burger");
--- src/com/gfarkas/pattern/builder/Main.java
+++ src/com/gfarkas/pattern/builder/Main.java
@@ -2,8 +2,17 @@ package com.gfarkas.pattern.builder;
 
 public class Main {
 
-    public static void main(String[] args) throws InvalidBurgerException {
-        Burger cheeseBurger = new Burger.BurgerBuilder("Cheeseburger", 0, Bun.NORMAL, Patty.BEEF, Cheese.EDAMI, Sauce.NORMAL)
-                .tomato(1).build();
+    public static void main(String[] args) {
+        Burger cheeseBurger =
+                new Burger.BurgerBuilder("Cheese Burger", 8, Bun.NORMAL, Patty.BEEF, Cheese.EDAMI, Sauce.NORMAL)
+                        .tomato(1).build();
+        Burger baconBurger =
+                new Burger.BurgerBuilder("Bacon Burger", 12, Bun.NORMAL, Patty.BEEF, Cheese.CHEDDAR, Sauce.NORMAL)
+                        .grilledBacon(2)
+                        .salad(true)
+                        .tomato(1)
+                        .redOnion(1)
+                        .cucumber(1)
+                        .build();
     }
 }
